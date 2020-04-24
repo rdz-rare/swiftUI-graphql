cec106bf5d857bc70e635331169201438bc649b4
Angel Rodriguez <rdz.rare@gmail.com>
2020-04-24 12:06:42 -0600

    Initial Commit

---
+++ grapql-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grapql-test
+//
+//  Created by Angel Rodriguez on 4/24/20.
+//  Copyright © 2020 Angel Rodriguez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
